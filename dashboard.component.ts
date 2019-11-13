670a453dcef0b986b1270c96d9af7db9060c3d20
Urvashi Sapate <42204796+urvashi27@users.noreply.github.com>
2019-11-13 22:54:59 +0530

    angular

---
+++ dashboard.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { StaticdataService } from '../staticdata.service';
+import { MatSort, MatTableDataSource, MatPaginator } from '@angular/material';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  displayedColumns: string[] = ['Name','Age','Salary','Experience'];
+  dataSource;
+  constructor(private service: StaticdataService) { }
+
+  ngOnInit() {
+  console.log(this.service.memberDetails)
+  
+  this.dataSource = new MatTableDataSource( this.service.memberDetails);
+  }
+
+}
